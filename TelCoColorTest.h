b8d58eb374668897194137bf869bc885a3f9bba9
gavaraipraveen <113239024+gavaraipraveen@users.noreply.github.com>
2024-07-29 10:55:12 +0530

    Update TelCoColorTest.h

--- TelCoColorTest.h
+++ TelCoColorTest.h
@@ -1,3 +1,4 @@
+#include "ColorCode.h"
 
 void testNumberToPair(int pairNumber, TelCoColorCoder::MajorColor expectedMajor, TelCoColorCoder::MinorColor expectedMinor);
 void testPairToNumber(TelCoColorCoder::MajorColor major, TelCoColorCoder::MinorColor minor, int expectedPairNumber);

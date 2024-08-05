4ca38425e2ac844c7ee7dcfba5fdd8769575c9cc
gavaraipraveen <113239024+gavaraipraveen@users.noreply.github.com>
2024-08-05 11:30:44 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,5 +7,6 @@ int main() {
     testPairToNumber(TelCoColorCoder::BLACK, TelCoColorCoder::ORANGE, 12);
     testPairToNumber(TelCoColorCoder::VIOLET, TelCoColorCoder::SLATE, 25);
 
+    printColorCodeManual();
     return 0;
 }
